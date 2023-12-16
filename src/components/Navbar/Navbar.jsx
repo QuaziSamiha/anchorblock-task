6107cc9541f7db57224553c90976a1e603dde362
QuaziSamiha <1703155@student.ruet.ac.bd>
2023-12-16 00:47:33 -0800

    react icon added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,9 @@
 /* eslint-disable no-undef */
 import { NavLink } from "react-router-dom";
-
+import { FaRegBell } from "react-icons/fa";
+import { CiSettings, CiSearch } from "react-icons/ci";
+import user from "../../assets/images/user.jpg";
+import "./Navbar.css";
 const Navbar = () => {
   const routes = [
     {
@@ -31,35 +34,47 @@ const Navbar = () => {
   ];
   return (
     <>
-      <section className="bg-[#6941c6] h-[72px] flex justify-between items-center">
-        <nav className="h-10">
-          <div className="flex justify-start">
-            <div>
-              <h1 className="h-6 w-14 font-bold text-xl text-white align-middle mt-2">Stack</h1>
+      <section className="bg-[#6941c6] h-[72px] flex items-center">
+        <nav className="h-10 flex w-[1234px] justify-between mx-20">
+          <div className="flex">
+            <div className="flex">
+              <img src={user} className="h-8 w-10 mx-2" alt="" />
+              <h1 className="h-6 w-14 font-bold text-xl text-white align-middle mt-2">
+                Stack
+              </h1>
             </div>
             <div className="flex">
               {routes.map((route) => (
-                <div
-                  key={route.id}
-                  className="inline shadow-xl text-center  shadow-[#503298] mx-2 h-10 rounded-md px-3"
+                <div key={route.id}
+                className="inline shadow-lg text-center shadow-[#503298] mx-2 h-10 rounded-md px-3"
                 >
                   <NavLink
                     to={route.path}
                     className="text-[#F4EBFF] text-base leading-6 font-medium align-text-middle"
                   >
-                    <span className="inline-block align-middle mt-2">
+                    <span className="mt-4 p-3 align-text-middle">
                       {route.name}
                     </span>
                   </NavLink>
                 </div>
               ))}
             </div>
-            <div className="flex">
-                <div className="h-10 w-10 mx-px shadow-sm shadow-[#503298]"></div>
-                <div className="h-10 w-10 mx-px shadow-sm shadow-[#503298]"></div>
-                <div className="h-10 w-10 mx-px shadow-sm shadow-[#503298]"></div>
-                <img className="h-10 w-10 mx-px shadow-sm shadow-[#503298]" src="" alt="" />
+          </div>
+          <div className="flex text-white">
+            <div className="h-10 w-10 mx-1 shadow-sm shadow-[#503298] flex justify-center items-center text-xl">
+              <CiSearch />
             </div>
+            <div className="h-10 w-10 mx-1 shadow-sm shadow-[#503298] flex justify-center items-center text-xl">
+              <CiSettings />
+            </div>
+            <div className="h-10 w-10 mx-1 shadow-sm shadow-[#503298] flex justify-center items-center text-xl">
+              <FaRegBell />
+            </div>
+            <img
+              className="h-10 w-10 mx-1 shadow-sm shadow-[#503298] rounded-full"
+              src={user}
+              alt=""
+            />
           </div>
         </nav>
       </section>

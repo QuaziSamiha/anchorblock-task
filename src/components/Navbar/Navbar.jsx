6fb087e12ba7376805a5372bdfba760cc2f3995d
QuaziSamiha <1703155@student.ruet.ac.bd>
2023-12-18 20:27:45 -0800

    navbar task done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom";
 import { FaRegBell } from "react-icons/fa";
 import { CiSettings, CiSearch } from "react-icons/ci";
 import user from "../../assets/images/user.jpg";
+import { PiStackDuotone } from "react-icons/pi";
 import "./Navbar.css";
 const Navbar = () => {
   const routes = [
@@ -19,40 +20,46 @@ const Navbar = () => {
     {
       id: 3,
       name: "Projects",
-      path: '/projects'
+      path: "/projects",
     },
     {
       id: 4,
       name: "Tasks",
-      path: 'tasks'
+      path: "tasks",
     },
     {
       id: 5,
       name: "Reporting",
-      path: '/reporting'
+      path: "/reporting",
+    },
+    {
+      id: 6,
+      name: "Dashboard",
+      path: "/dashboard",
     },
   ];
   return (
     <>
       <section className="bg-[#6941c6] h-[72px] flex items-center">
-        <nav className="h-10 flex w-[1234px] justify-between mx-20">
+        <nav className="h-10 flex w-[1234px] justify-between mx-16">
           <div className="flex">
             <div className="flex">
-              <img src={user} className="h-8 w-10 mx-2" alt="" />
+              <PiStackDuotone className="h-8 w-10 mx-2 text-white mt-2" />
               <h1 className="h-6 w-14 font-bold text-xl text-white align-middle mt-2">
                 Stack
               </h1>
             </div>
             <div className="flex">
               {routes.map((route) => (
-                <div key={route.id}
-                className="inline shadow-lg text-center shadow-[#503298] mx-2 h-10 rounded-md px-3"
+                <div
+                  key={route.id}
+                  className="inline shadow-lg text-center shadow-[#503298] mx-1 h-10 rounded-md"
                 >
                   <NavLink
                     to={route.path}
                     className="text-[#F4EBFF] text-base leading-6 font-medium"
                   >
-                    <p className="pt-2 h-10 rounded-md align-middle">
+                    <p className="pt-2 h-10 rounded-md align-middle  px-3">
                       {route.name}
                     </p>
                   </NavLink>
